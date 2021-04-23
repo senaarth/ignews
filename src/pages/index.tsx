b23b09f7c502c90caad3679e5ad22176e601717e
senaarth <senaarth@gmail.com>
2021-04-23 18:57:56 -0300

    styles home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,24 @@
+import Head from "next/head";
+
+import styles from "../styles/home.module.scss";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏🏾 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the articles<br/>
+            <span>for $9.90 monthly</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
+    </>
+  );
 }
