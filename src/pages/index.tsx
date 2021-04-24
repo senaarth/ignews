f5907d4232880f1ef6d45d5694000b230ba2c218
senaarth <senaarth@gmail.com>
2021-04-23 21:41:20 -0300

    uses SSR to get subscription info from stripe API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "../styles/home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,12 +27,30 @@ export default function Home() {
           <p>
             Get access to all the articles
             <br />
-            <span>for $9.90 monthly</span>
+            <span>for {product.amount} monthly</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1IjZCyGgnPeqWVymqp1HgUXA");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
