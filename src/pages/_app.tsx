b4ec20cd93d517b6e29ab556bd642e691f623e0d
senaarth <senaarth@gmail.com>
2021-06-08 19:25:03 -0300

    ads github nextauth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
+import { Provider as AuthProvider } from "next-auth/client";
 
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <AuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </AuthProvider>
   );
 }
 
