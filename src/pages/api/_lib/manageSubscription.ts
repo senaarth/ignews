0d9739c44b1799f53027ea504b7f5bd756f4766d
senaarth <senaarth@gmail.com>
2021-06-10 19:27:01 -0300

    listens and treats other event types

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -4,22 +4,35 @@ import { stripe } from "../../../services/stripe";
 
 export async function saveSubscription(
   subscriptionId: string,
-  customerId: string
+  customerId: string,
+  createAction = false
 ) {
-    const userRef = await fauna.query(
-      q.Select("ref", q.Get(q.Match(q.Index("userByCustomerId"), customerId)))
-    );
+  const userRef = await fauna.query(
+    q.Select("ref", q.Get(q.Match(q.Index("userByCustomerId"), customerId)))
+  );
 
-    const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
 
-    const subscriptionData = {
-      id: subscription.id,
-      userId: userRef,
-      status: subscription.status,
-      priceId: subscription.items.data[0].price.id,
-    };
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    priceId: subscription.items.data[0].price.id,
+  };
 
+  if (createAction) {
     await fauna.query(
       q.Create(q.Collection("subscriptions"), { data: subscriptionData })
     );
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref",
+          q.Get(q.Match(q.Index("subscriptionById"), subscriptionId))
+        ),
+        { data: subscriptionData }
+      )
+    );
+  }
 }
