b3cd6bf7010d18c14d8ed3ae8a4812af0006a47f
senaarth <senaarth@gmail.com>
2021-06-10 21:36:14 -0300

    highlights active page on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { SignInButton } from "../SignInButton/";
+import { ActiveLink } from "../ActiveLink/";
 
 import styles from "./styles.module.scss";
 
@@ -6,10 +9,20 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news logo" />
+        <ActiveLink activeClassName={styles.active} prefetch href="/">
+          <img
+            src="/images/logo.svg"
+            alt="ig.news logo"
+            style={{ cursor: "pointer" }}
+          />
+        </ActiveLink>
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} prefetch href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} prefetch href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
